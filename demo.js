2d99baf9e1443b6e873aaeb3b0557f08dd7abfc8
BeeHuang <beehuang999@gmail.com>
2018-07-30 23:36:22 +0800

    change SeoCheck to class add setStrongLimitCount function

--- demo.js
+++ demo.js
@@ -1,28 +1,32 @@
 var fs = require('fs');
 var SeoCheck = require( './seo.js' );
 
+
+
 // demo 1
 // 1. read from filepath
-// 2. check rule 3, 4, 5
+// 2. set StringLimitCount = 3
+// 2. check rule 3, 4, 5 and with one error rule 10
 // 3. output 3 console
 (async function() {
-    SeoCheck.SeoCheck();
-    await SeoCheck.setFilePath('test.html');
-    SeoCheck.check('3,4,5');
-    SeoCheck.setOutputMethond(3);
-    console.log(SeoCheck.getOutput());
+    const seoCheck = new SeoCheck();
+    await seoCheck.setFilePath('test.html');
+    seoCheck.setStrongLimitCount(3);
+    seoCheck.check('3,4,5,10');
+    seoCheck.setOutputMethond(3);
+    console.log(seoCheck.getOutput());
 }());
 
 // demo 2
 // 1. read from readable stream
-// 2. check rule 3, 4, 5
+// 2. check rule 1,2,3, 4, 5
 // 3. output A file (User is able to config the output destination)
 
 // (async function() {
-//     SeoCheck.SeoCheck();
+//     const seoCheck = new SeoCheck();
 //     const stream = fs.createReadStream('./writeable.html');
-//     await SeoCheck.setReadableStream(stream);
-//     SeoCheck.check('1,2,3,4,5');
-//     SeoCheck.setOutputMethond(1, 'outPut.txt');
-//     console.log(SeoCheck.getOutput());
+//     await seoCheck.setReadableStream(stream);
+//     seoCheck.check('1,2,3,4,5');
+//     seoCheck.setOutputMethond(1, 'outPut.txt');
+//     console.log(seoCheck.getOutput());
 // }());
